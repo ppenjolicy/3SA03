649d1ece8138e348a39716d3834fdc117bcdb260
ppenjolicy <yurin.pk.123@gmail.com>
2018-09-13 10:36:32 +0700

    4.2styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value="h" />
-        <CharacterCard value="i" />
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+        }
       </div>
     );
   }
